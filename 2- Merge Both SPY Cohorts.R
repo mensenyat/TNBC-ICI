34663cf9d23c925687b9ead8102d50d3165d1f74
mensenyat <46361666+mensenyat@users.noreply.github.com>
2023-01-19 12:11:18 +0100

    Update 2- Merge Both SPY Cohorts.R

--- 2- Merge Both SPY Cohorts.R
+++ 2- Merge Both SPY Cohorts.R
@@ -6,20 +6,19 @@ library(ggplot2)
 library(Rtsne)
 library(sva)
 
-Ola = read.table("GSE173839_data.txt", sep = "\t", header = T, row.names = 1)
+Durva = read.table("GSE173839_data.txt", sep = "\t", header = T, row.names = 1)
 genes = read.table("Probes_to_genes_updated.txt", sep = "\t", header = T)
-Ola = merge(genes, Ola, by.x = 1, by.y = 0)
-rownames(Ola) <- make.names(Ola$GeneName, unique = TRUE)
-Ola = Ola[,-c(1:4)]
-Ola = data.frame(apply(Ola, 2, function(x) as.numeric(as.character(x))), row.names = rownames(Ola))
+Durva = merge(genes, Durva, by.x = 1, by.y = 0)
+rownames(Durva) <- make.names(Durva$GeneName, unique = TRUE)
+Durva = Durva[,-c(1:4)]
+Durva = data.frame(apply(Durva, 2, function(x) as.numeric(as.character(x))), row.names = rownames(Durva))
 
-metaOla = read.table("Metadata.txt", sep = "\t", header = T)
-metaOla = metaOla[which(metaOla$Hormone == "TNBC"),]
-metaOla = metaOla[which(metaOla$X.Sample_characteristics_ch1.4 == "arm: durvalumab/olaparib"),]
+metaDurva = read.table("Metadata.txt", sep = "\t", header = T)
+metaDurva = metaDurva[which(metaDurva$Hormone == "TNBC"),]
+metaDurva = metaDurva[which(metaDurva$X.Sample_characteristics_ch1.4 == "arm: durvalumab/olaparib"),]
 
-Ola = Ola[,which(colnames(Ola) %in% metaOla$Patient)]
+Durva = Durva[,which(colnames(Durva) %in% metaDurva$Patient)]
 
-setwd("C:/Users/miki7/OneDrive/R/BRCA/SMARCA4/TONIC/ORR Signature")
 Pembro = read.table("GeneExp_SPY2_Neoadjuvant_GPL20078_Corrected.txt", sep = "\t", header = T, row.names = 1)
 metaPembro = read.table("Clinical_Data_GPL20078.txt", sep = "\t", header = T)
 metaPembro = metaPembro[which(metaPembro$HR == 0 & metaPembro$HER2 == 0),]
@@ -31,11 +30,11 @@ Pembro = Pembro[,which(colnames(Pembro) %in% rownames(metaPembro))]
 Pembro = data.frame(apply(Pembro, 2, function(x) as.numeric(as.character(x))), row.names = rownames(Pembro))
 
 # Select only genes in both #
-Ola = Ola[which(rownames(Ola) %in% rownames(Pembro)),]
-Pembro = Pembro[which(rownames(Pembro) %in% rownames(Ola)),]
-Ola = Ola[order(match(rownames(Ola), rownames(Pembro))),]
+Durva = Durva[which(rownames(Durva) %in% rownames(Pembro)),]
+Pembro = Pembro[which(rownames(Pembro) %in% rownames(Durva)),]
+Durva = Durva[order(match(rownames(Durva), rownames(Pembro))),]
 Pembrot = as.data.frame(as.matrix(t(Pembro)))
-Olat = as.data.frame(as.matrix(t(Ola)))
+Durvat = as.data.frame(as.matrix(t(Durva)))
 
 #### Correct batch effect ####
 # Merge all datasets #
@@ -45,7 +44,7 @@ spy = spy[,-1]
 spy = na.omit(spy)
 
 cb.df.mdata <- cbind.data.frame("sample" = colnames(spy), # exclude uid column, c(1)
-                                "batch" = c(rep("SPY-Ola", ncol(spy)), rep("SPY-Pembro", ncol(Pembro))))
+                                "batch" = c(rep("SPY-Durva", ncol(spy)), rep("SPY-Pembro", ncol(Pembro))))
 
 cb.corr.model <- model.matrix(~1, data = cb.df.mdata)
 
@@ -75,14 +74,13 @@ dev.off()
 write.table(data_corrected, file = "Both SPY corrected.txt", sep = "\t")
 
 #### Merge clinical data ####
-setwd("C:/Users/miki7/OneDrive/R/BRCA/SMARCA4/TONIC/ORR Signature")
-metaOla = read.table("Metadata.txt", sep = "\t", header = T)
-metaOla = metaOla[which(metaOla$Hormone == "TNBC"),]
-metaOla = metaOla[which(metaOla$X.Sample_characteristics_ch1.4 == "arm: durvalumab/olaparib"),]
+metaDurva = read.table("Metadata SPY-Durvalumab.txt", sep = "\t", header = T)
+metaDurva = metaDurva[which(metaDurva$Hormone == "TNBC"),]
+metaDurva = metaDurva[which(metaDurva$X.Sample_characteristics_ch1.4 == "arm: durvalumab/olaparib"),]
 
 metaBoth = as.data.frame(cbind("Patient" = c(metaSPY$Patient, rownames(meta)),
-                           "Response" = c(metaSPY$Response, meta$Response),
-                           "Batch" = c(rep("Olaparib", nrow(metaSPY)), rep("Pembro", nrow(meta)))))
+                               "Response" = c(metaSPY$Response, meta$Response),
+                               "Batch" = c(rep("Durva", nrow(metaSPY)), rep("Pembro", nrow(meta)))))
 metaBoth = metaBoth[order(match(metaBoth$Patient, colnames(data_corrected))),]
 
 write.table(metaBoth, file = "Meta_both_SPY_cohorts.txt", sep = "\t", row.names = F)
